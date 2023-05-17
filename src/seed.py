01f1eec6d7ad8a41453fe56e8ee93f0107e5a005
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-05-16 20:28:51 -0400

    remove seed file

--- src/seed.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-conn_string = 'postgresql://localhost/yelp_lunch'
-import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/ledeprogram/courses/master/foundations/mapping/tilemill/yelp-lunch-nyc.csv')
-conn = create_engine(conn_string)
-df.to_sql('restaurants', conn, if_exists='replace', index_label = 'id')
